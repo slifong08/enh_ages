489352fb73773af97a604d50e52bf5f0f2245978
slifong08 <slfong08@gmail.com>
2021-06-08 13:55:17 -0700

    updated config file with names of each file used in this project

--- tyler/evolutionary_conservation/config.py
+++ tyler/evolutionary_conservation/config.py
@@ -1,1 +1,20 @@
 # all the files I have used for this human acceleration analysis:
+
+
+#"/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/species_specific_10k/"
+
+# all elements
+all = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/all/GG-LL_all_OCRs.bed"
+hu_specific = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/hu_specific/GM12878_specific.bed"
+rhe_specific = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/rhe_specific/LCL8664_specific.bed"
+
+# subtracting TEs
+all_noTE = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/all/subtract_te/all_wo_te.bed"
+hu_specific_noTE = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/hu_specific/subtract_te/hu-specific_wo_te.bed"
+rhe_specific_noTE = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/rhe_specific/subtract_te/rhe-specific_wo_te.bed"
+
+# positive control
+hars = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/hars/hars_merged_PMIDs_hg38.bed"
+
+# negative control
+phastCons = "/dors/capra_lab/users/fongsl/tyler/data/CON_ACC/phastCons/phastConsElements100way_hg38.bed.gz"
